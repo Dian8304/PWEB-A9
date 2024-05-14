da1daff259f35e0c0a07204cee75f94b973d6bfa
Dian8304 <felisita8304dian@gmail.com>
2024-05-14 15:45:31 +0700

    1st commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'https://localhost/PWEB-A9/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'dbpagari');
